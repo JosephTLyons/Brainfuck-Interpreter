4d2599acf92c6fe96c824e29e0d795fdf05326b6
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-12-19 16:26:11 -0500

    GUI changes

--- Source/GUIComponent.cpp
+++ Source/GUIComponent.cpp
@@ -40,6 +40,7 @@ GUIComponent::GUIComponent ()
     brainfuckCodeEditor->setCaretVisible (true);
     brainfuckCodeEditor->setPopupMenuEnabled (true);
     brainfuckCodeEditor->setColour (TextEditor::textColourId, Colours::white);
+    brainfuckCodeEditor->setColour (TextEditor::highlightColourId, Colour (0xbfffffff));
     brainfuckCodeEditor->setText (String());
 
     addAndMakeVisible (inputEditor = new TextEditor ("inputEditor"));
@@ -50,31 +51,66 @@ GUIComponent::GUIComponent ()
     inputEditor->setCaretVisible (true);
     inputEditor->setPopupMenuEnabled (true);
     inputEditor->setColour (TextEditor::textColourId, Colours::white);
+    inputEditor->setColour (TextEditor::highlightColourId, Colour (0xbfffffff));
     inputEditor->setText (String());
 
     addAndMakeVisible (outputEditor = new TextEditor ("outputEditor"));
     outputEditor->setMultiLine (false);
     outputEditor->setReturnKeyStartsNewLine (false);
-    outputEditor->setReadOnly (false);
+    outputEditor->setReadOnly (true);
     outputEditor->setScrollbarsShown (true);
-    outputEditor->setCaretVisible (true);
+    outputEditor->setCaretVisible (false);
     outputEditor->setPopupMenuEnabled (true);
     outputEditor->setColour (TextEditor::textColourId, Colours::white);
+    outputEditor->setColour (TextEditor::highlightColourId, Colour (0xbfffffff));
     outputEditor->setText (String());
 
     addAndMakeVisible (errorMessagesEditor = new TextEditor ("errorMessagesEditor"));
     errorMessagesEditor->setMultiLine (true);
     errorMessagesEditor->setReturnKeyStartsNewLine (true);
-    errorMessagesEditor->setReadOnly (false);
+    errorMessagesEditor->setReadOnly (true);
     errorMessagesEditor->setScrollbarsShown (true);
-    errorMessagesEditor->setCaretVisible (true);
+    errorMessagesEditor->setCaretVisible (false);
     errorMessagesEditor->setPopupMenuEnabled (true);
     errorMessagesEditor->setColour (TextEditor::textColourId, Colours::white);
+    errorMessagesEditor->setColour (TextEditor::highlightColourId, Colour (0xbfffffff));
     errorMessagesEditor->setText (String());
 
     addAndMakeVisible (executeCodeButton = new TextButton ("executeCodeButton"));
     executeCodeButton->setButtonText (TRANS("Execute"));
     executeCodeButton->addListener (this);
+
+    addAndMakeVisible (brainfuckCodeLabel = new Label ("brainfuckCodeLabel",
+                                                       TRANS("Brainfuck Code:")));
+    brainfuckCodeLabel->setFont (Font (15.00f, Font::plain).withTypefaceStyle ("Regular"));
+    brainfuckCodeLabel->setJustificationType (Justification::centredLeft);
+    brainfuckCodeLabel->setEditable (false, false, false);
+    brainfuckCodeLabel->setColour (TextEditor::textColourId, Colours::black);
+    brainfuckCodeLabel->setColour (TextEditor::backgroundColourId, Colour (0x00000000));
+
+    addAndMakeVisible (inputLabel = new Label ("inputLabel",
+                                               TRANS("Input:")));
+    inputLabel->setFont (Font (15.00f, Font::plain).withTypefaceStyle ("Regular"));
+    inputLabel->setJustificationType (Justification::centredLeft);
+    inputLabel->setEditable (false, false, false);
+    inputLabel->setColour (TextEditor::textColourId, Colours::black);
+    inputLabel->setColour (TextEditor::backgroundColourId, Colour (0x00000000));
+
+    addAndMakeVisible (outputLabel = new Label ("outputLabel",
+                                                TRANS("Output:")));
+    outputLabel->setFont (Font (15.00f, Font::plain).withTypefaceStyle ("Regular"));
+    outputLabel->setJustificationType (Justification::centredLeft);
+    outputLabel->setEditable (false, false, false);
+    outputLabel->setColour (TextEditor::textColourId, Colours::black);
+    outputLabel->setColour (TextEditor::backgroundColourId, Colour (0x00000000));
+
+    addAndMakeVisible (errorsAndWarningsLabel = new Label ("errorsAndWarningsLabel",
+                                                           TRANS("Errors and Warnings:")));
+    errorsAndWarningsLabel->setFont (Font (15.00f, Font::plain).withTypefaceStyle ("Regular"));
+    errorsAndWarningsLabel->setJustificationType (Justification::centredLeft);
+    errorsAndWarningsLabel->setEditable (false, false, false);
+    errorsAndWarningsLabel->setColour (TextEditor::textColourId, Colours::black);
+    errorsAndWarningsLabel->setColour (TextEditor::backgroundColourId, Colour (0x00000000));
 
 
     //[UserPreSize]
@@ -84,6 +120,7 @@ GUIComponent::GUIComponent ()
 
 
     //[Constructor] You can add your own custom stuff here..
+
     //[/Constructor]
 }
 
@@ -97,6 +134,10 @@ GUIComponent::~GUIComponent()
     outputEditor = nullptr;
     errorMessagesEditor = nullptr;
     executeCodeButton = nullptr;
+    brainfuckCodeLabel = nullptr;
+    inputLabel = nullptr;
+    outputLabel = nullptr;
+    errorsAndWarningsLabel = nullptr;
 
 
     //[Destructor]. You can add your own custom destruction code here..
@@ -120,11 +161,15 @@ void GUIComponent::resized()
     //[UserPreResize] Add your own custom resize code here..
     //[/UserPreResize]
 
-    brainfuckCodeEditor->setBounds (0, 64, 600, 100);
-    inputEditor->setBounds (0, 198, 600, 30);
-    outputEditor->setBounds (0, 246, 600, 30);
-    errorMessagesEditor->setBounds (0, 294, 600, 100);
-    executeCodeButton->setBounds (216, 168, 150, 24);
+    brainfuckCodeEditor->setBounds (0, 35, 600, 100);
+    inputEditor->setBounds (0, 170, 600, 30);
+    outputEditor->setBounds (0, 235, 600, 30);
+    errorMessagesEditor->setBounds (0, 300, 600, 100);
+    executeCodeButton->setBounds (225, 140, 150, 25);
+    brainfuckCodeLabel->setBounds (0, 5, 150, 25);
+    inputLabel->setBounds (0, 140, 150, 25);
+    outputLabel->setBounds (0, 205, 150, 25);
+    errorsAndWarningsLabel->setBounds (0, 270, 150, 25);
     //[UserResized] Add your own custom resize handling here..
     //[/UserResized]
 }
@@ -137,11 +182,14 @@ void GUIComponent::buttonClicked (Button* buttonThatWasClicked)
     if (buttonThatWasClicked == executeCodeButton)
     {
         //[UserButtonCode_executeCodeButton] -- add your button handler code here..
-        
+        interpreter.setInputText (inputEditor->getText());
         interpreter.parseText (brainfuckCodeEditor->getText());
         outputEditor->setText (interpreter.getOutputText());
+        errorMessagesEditor->setText (interpreter.getWarningText());
         interpreter.reset();
-        
+        outputEditor->clear();
+        errorMessagesEditor->clear();
+
         //[/UserButtonCode_executeCodeButton]
     }
 
@@ -170,24 +218,44 @@ BEGIN_JUCER_METADATA
                  fixedSize="1" initialWidth="600" initialHeight="400">
   <BACKGROUND backgroundColour="ff505050"/>
   <TEXTEDITOR name="brainfuckCodeEditor" id="59d491ad053b0a3f" memberName="brainfuckCodeEditor"
-              virtualName="" explicitFocusOrder="0" pos="0 64 600 100" textcol="ffffffff"
-              initialText="" multiline="1" retKeyStartsLine="1" readonly="0"
-              scrollbars="1" caret="1" popupmenu="1"/>
+              virtualName="" explicitFocusOrder="0" pos="0 35 600 100" textcol="ffffffff"
+              hilitecol="bfffffff" initialText="" multiline="1" retKeyStartsLine="1"
+              readonly="0" scrollbars="1" caret="1" popupmenu="1"/>
   <TEXTEDITOR name="inputEditor" id="ce63d681197cb556" memberName="inputEditor"
-              virtualName="" explicitFocusOrder="0" pos="0 198 600 30" textcol="ffffffff"
-              initialText="" multiline="0" retKeyStartsLine="0" readonly="0"
-              scrollbars="1" caret="1" popupmenu="1"/>
+              virtualName="" explicitFocusOrder="0" pos="0 170 600 30" textcol="ffffffff"
+              hilitecol="bfffffff" initialText="" multiline="0" retKeyStartsLine="0"
+              readonly="0" scrollbars="1" caret="1" popupmenu="1"/>
   <TEXTEDITOR name="outputEditor" id="e6b0e684a57da253" memberName="outputEditor"
-              virtualName="" explicitFocusOrder="0" pos="0 246 600 30" textcol="ffffffff"
-              initialText="" multiline="0" retKeyStartsLine="0" readonly="0"
-              scrollbars="1" caret="1" popupmenu="1"/>
+              virtualName="" explicitFocusOrder="0" pos="0 235 600 30" textcol="ffffffff"
+              hilitecol="bfffffff" initialText="" multiline="0" retKeyStartsLine="0"
+              readonly="1" scrollbars="1" caret="0" popupmenu="1"/>
   <TEXTEDITOR name="errorMessagesEditor" id="d9c721f19ad3d913" memberName="errorMessagesEditor"
-              virtualName="" explicitFocusOrder="0" pos="0 294 600 100" textcol="ffffffff"
-              initialText="" multiline="1" retKeyStartsLine="1" readonly="0"
-              scrollbars="1" caret="1" popupmenu="1"/>
+              virtualName="" explicitFocusOrder="0" pos="0 300 600 100" textcol="ffffffff"
+              hilitecol="bfffffff" initialText="" multiline="1" retKeyStartsLine="1"
+              readonly="1" scrollbars="1" caret="0" popupmenu="1"/>
   <TEXTBUTTON name="executeCodeButton" id="e8581b84cdf8cec8" memberName="executeCodeButton"
-              virtualName="" explicitFocusOrder="0" pos="216 168 150 24" buttonText="Execute"
+              virtualName="" explicitFocusOrder="0" pos="225 140 150 25" buttonText="Execute"
               connectedEdges="0" needsCallback="1" radioGroupId="0"/>
+  <LABEL name="brainfuckCodeLabel" id="5421b93948ba369c" memberName="brainfuckCodeLabel"
+         virtualName="" explicitFocusOrder="0" pos="0 5 150 25" edTextCol="ff000000"
+         edBkgCol="0" labelText="Brainfuck Code:" editableSingleClick="0"
+         editableDoubleClick="0" focusDiscardsChanges="0" fontname="Default font"
+         fontsize="15" kerning="0" bold="0" italic="0" justification="33"/>
+  <LABEL name="inputLabel" id="e04ec47e05f2378c" memberName="inputLabel"
+         virtualName="" explicitFocusOrder="0" pos="0 140 150 25" edTextCol="ff000000"
+         edBkgCol="0" labelText="Input:" editableSingleClick="0" editableDoubleClick="0"
+         focusDiscardsChanges="0" fontname="Default font" fontsize="15"
+         kerning="0" bold="0" italic="0" justification="33"/>
+  <LABEL name="outputLabel" id="f84f3698f64703b8" memberName="outputLabel"
+         virtualName="" explicitFocusOrder="0" pos="0 205 150 25" edTextCol="ff000000"
+         edBkgCol="0" labelText="Output:" editableSingleClick="0" editableDoubleClick="0"
+         focusDiscardsChanges="0" fontname="Default font" fontsize="15"
+         kerning="0" bold="0" italic="0" justification="33"/>
+  <LABEL name="errorsAndWarningsLabel" id="af33826739efbb85" memberName="errorsAndWarningsLabel"
+         virtualName="" explicitFocusOrder="0" pos="0 270 150 25" edTextCol="ff000000"
+         edBkgCol="0" labelText="Errors and Warnings:" editableSingleClick="0"
+         editableDoubleClick="0" focusDiscardsChanges="0" fontname="Default font"
+         fontsize="15" kerning="0" bold="0" italic="0" justification="33"/>
 </JUCER_COMPONENT>
 
 END_JUCER_METADATA
