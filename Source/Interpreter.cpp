11205fc20d8be6be2b4b89b305a089f385005815
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-12-19 13:09:38 -0500

    Working on interpreter

--- Source/Interpreter.cpp
+++ Source/Interpreter.cpp
@@ -6,3 +6,81 @@
 //
 
 #include "Interpreter.hpp"
+
+Interpreter::Interpreter()
+{
+    tapeArray.resize (30000);
+    tapeArray.fill (0);
+    
+    index = 0;
+}
+
+void Interpreter::parseText (const String &brainfuckCode)
+{
+    for (int i = 0; i < brainfuckCode.length(); i++)
+    {
+        if (i == '+')
+            incrementCellValue();
+        
+        else if (i == '-')
+            decrementCellValue();
+        
+        else if (i == '>')
+            incrementIndex();
+        
+        else if (i == '<')
+            decrementIndex();
+        
+        else if (i == ',')
+            acceptInput();
+        
+        else if (i == '.')
+            printOutput();
+        
+        else if (i == '[')
+            openBracket();
+        
+        else if (i == ']')
+            closedBracket();
+    }
+}
+
+void Interpreter::incrementCellValue()
+{
+    
+}
+
+void Interpreter::decrementCellValue()
+{
+    
+}
+
+void Interpreter::incrementIndex()
+{
+    
+}
+
+void Interpreter::decrementIndex()
+{
+    
+}
+
+void Interpreter::acceptInput()
+{
+    
+}
+
+void Interpreter::printOutput()
+{
+    
+}
+
+void Interpreter::openBracket()
+{
+    
+}
+
+void Interpreter::closedBracket()
+{
+    
+}
--- Source/Interpreter.hpp
+++ Source/Interpreter.hpp
@@ -8,10 +8,27 @@
 #ifndef Interpreter_hpp
 #define Interpreter_hpp
 
+#include "../JuceLibraryCode/JuceHeader.h"
+
 class Interpreter
 {
 private:
+    Array<char> tapeArray;
+    int index;
+    
 public:
+    Interpreter();
+    
+    void parseText (const String &brainfuckCode);
+    
+    void incrementCellValue();
+    void decrementCellValue();
+    void incrementIndex();
+    void decrementIndex();
+    void acceptInput();
+    void printOutput();
+    void openBracket();
+    void closedBracket();
     
 };
 
