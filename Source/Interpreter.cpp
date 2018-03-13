d759b4199d23cbdcc9ec68631d8c5e09d489d6d9
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-03-12 23:10:07 -0400

    Removed logger statements

--- Source/Interpreter.cpp
+++ Source/Interpreter.cpp
@@ -40,8 +40,6 @@ void Interpreter::decrementCellValue()
 {
     if (tapeArray[tapeArrayIndex] == 0)
         warningText += "Warning: Negative overflow occured at cell " + (String) tapeArrayIndex + ".\n";
-    
-    Logger::writeToLog((String) tapeArray[tapeArrayIndex]);
 
     tapeArray.set (tapeArrayIndex, tapeArray[tapeArrayIndex] - 1);
 }
